5ff7fe4c896bdd44c7627caea2758eab59760373
Scpter <jixhyspersonalemail@gmail.com>
2026-05-30 13:43:12 +0800

    Basic assertion and logging system

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,13 @@
-#include <test.h>
+#include <core/logger.h>
+#include <core/asserts.h>
 
 int main(void) {
-    print_int(42);
+    KFATAL("A test message: %f", 3.14f);
+    KERROR("Error test message: %f", 3.14f);
+    KWARN("Warn test message: %f", 3.14f);
+    KINFO("Info test message: %f", 3.14f);
+    KDEBUG("Debug test message: %f", 3.14f);
+    KTRACE("Trace test message: %f", 3.14f);
+    KASSERT(1 == 0);
     return 0;
 }
