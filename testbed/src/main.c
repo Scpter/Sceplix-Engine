75c01bdad1733810d03f5748f3d2a82868cf5722
Scpter <jixhyspersonalemail@gmail.com>
2026-05-31 14:18:47 +0800

    Windows platform layer

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,9 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+// Test
+#include <platform/platform.h>
+
 int main(void) {
     KFATAL("A test message: %f", 3.14f);
     KERROR("Error test message: %f", 3.14f);
@@ -8,6 +11,15 @@ int main(void) {
     KINFO("Info test message: %f", 3.14f);
     KDEBUG("Debug test message: %f", 3.14f);
     KTRACE("Trace test message: %f", 3.14f);
-    KASSERT(1 == 0);
+    // KASSERT(1 == 0);
+
+    platform_state state;
+    if (platform_startup(&state, "Sceplix Engine First Run", 100, 100, 1280, 720)) {
+        while (TRUE) {
+           platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
+
     return 0;
 }
